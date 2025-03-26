452f83167cfd4b585e248f3e4f0d110bf8511c16
André Bierlein <andre@bierlein.email>
2025-03-26 19:44:24 +0100

    Add cubit to contain syncable state

---
+++ lib/repositories/redis_repository.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:redis/redis.dart';
+
+class RedisRepository {
+  final String host;
+  final int port;
+
+  RedisRepository({required this.host, required this.port});
+
+  // TODO: create simple connection pool
+  // TODO: add function here to query state (to fully encapsulate redis logic)
+  // TODO: determine redis host based on platform
+
+  Future<Command> connect() async {
+    final con = RedisConnection();
+    return await con.connect(host, port);
+  }
+
+  Stream<(String, String)> subscribe(String channel) async* {
+    final cmd = await connect();
+
+    final ps = PubSub(cmd);
+    ps.subscribe([channel]);
+
+    try {
+      yield* ps
+          .getStream()
+          .map((msg) {
+        if (msg is List && msg.length >= 3 && msg[0] == 'message') {
+          return (msg[1].toString(), msg[2].toString());
+        }
+
+        return null;
+      })
+          .where((result) => result != null)
+          .map((rec) => rec!);
+    } finally {
+      await cmd.get_connection().close();
+    }
+  }
+}
