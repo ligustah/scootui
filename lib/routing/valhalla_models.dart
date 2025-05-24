fe5e3ad4dbf8821470c32c706d5e3773a0deb2a6
André Bierlein <andre@bierlein.email>
2025-05-24 22:37:39 +0200

    Use valhalla for routing

---
+++ lib/routing/valhalla_models.dart
@@ -0,0 +1,58 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:latlong2/latlong.dart';
+
+part 'valhalla_models.freezed.dart';
+part 'valhalla_models.g.dart';
+
+@freezed
+abstract class ValhallaResponse with _$ValhallaResponse {
+  const factory ValhallaResponse({
+    required Trip trip,
+  }) = _ValhallaResponse;
+
+  factory ValhallaResponse.fromJson(Map<String, dynamic> json) =>
+      _$ValhallaResponseFromJson(json);
+}
+
+@freezed
+abstract class Trip with _$Trip {
+  const factory Trip({
+    required List<Leg> legs,
+  }) = _Trip;
+
+  factory Trip.fromJson(Map<String, dynamic> json) => _$TripFromJson(json);
+}
+
+@freezed
+abstract class Leg with _$Leg {
+  const factory Leg({
+    required List<Maneuver> maneuvers,
+    required Summary summary,
+    required String shape,
+  }) = _Leg;
+
+  factory Leg.fromJson(Map<String, dynamic> json) => _$LegFromJson(json);
+}
+
+@freezed
+abstract class Maneuver with _$Maneuver {
+  const factory Maneuver({
+    required int type,
+    required double length,
+    int? beginShapeIndex,
+  }) = _Maneuver;
+
+  factory Maneuver.fromJson(Map<String, dynamic> json) =>
+      _$ManeuverFromJson(json);
+}
+
+@freezed
+abstract class Summary with _$Summary {
+  const factory Summary({
+    required double length,
+    required int time,
+  }) = _Summary;
+
+  factory Summary.fromJson(Map<String, dynamic> json) =>
+      _$SummaryFromJson(json);
+}
---
+++ lib/routing/valhalla_models.g.dart
@@ -0,0 +1,63 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'valhalla_models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_ValhallaResponse _$ValhallaResponseFromJson(Map<String, dynamic> json) =>
+    _ValhallaResponse(
+      trip: Trip.fromJson(json['trip'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$ValhallaResponseToJson(_ValhallaResponse instance) =>
+    <String, dynamic>{
+      'trip': instance.trip,
+    };
+
+_Trip _$TripFromJson(Map<String, dynamic> json) => _Trip(
+      legs: (json['legs'] as List<dynamic>)
+          .map((e) => Leg.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$TripToJson(_Trip instance) => <String, dynamic>{
+      'legs': instance.legs,
+    };
+
+_Leg _$LegFromJson(Map<String, dynamic> json) => _Leg(
+      maneuvers: (json['maneuvers'] as List<dynamic>)
+          .map((e) => Maneuver.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      summary: Summary.fromJson(json['summary'] as Map<String, dynamic>),
+      shape: json['shape'] as String,
+    );
+
+Map<String, dynamic> _$LegToJson(_Leg instance) => <String, dynamic>{
+      'maneuvers': instance.maneuvers,
+      'summary': instance.summary,
+      'shape': instance.shape,
+    };
+
+_Maneuver _$ManeuverFromJson(Map<String, dynamic> json) => _Maneuver(
+      type: (json['type'] as num).toInt(),
+      length: (json['length'] as num).toDouble(),
+      beginShapeIndex: (json['beginShapeIndex'] as num?)?.toInt(),
+    );
+
+Map<String, dynamic> _$ManeuverToJson(_Maneuver instance) => <String, dynamic>{
+      'type': instance.type,
+      'length': instance.length,
+      'beginShapeIndex': instance.beginShapeIndex,
+    };
+
+_Summary _$SummaryFromJson(Map<String, dynamic> json) => _Summary(
+      length: (json['length'] as num).toDouble(),
+      time: (json['time'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$SummaryToJson(_Summary instance) => <String, dynamic>{
+      'length': instance.length,
+      'time': instance.time,
+    };
